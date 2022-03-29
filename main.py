99ef1a3de57861648c866def44f8e644ad278e17
Jake Gully <jake@maya.agency>
2022-03-30 00:23:47 +0100

    Seems to basically work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from html.parser import HTMLParser
+from urllib.parse import urlparse
 import argparse
 import requests
 
@@ -15,47 +16,91 @@ urls = []
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if (tag == 'a'):
-            add_url(attrs["href"])
+            for name, value in attrs:
+                if name == 'href':
+                    add_url(value)
 
 
 parser = MyHTMLParser()
 
 
 def add_url(url):
+    if (url[0:1] == "/"):
+        url = base_url + url
+
     try:
-        existing = next(x for x in urls if x.url == url)
+        existing = next(x for x in urls if x["url"] == url)
     except StopIteration:
         existing = None
 
     if (existing):
         return
 
-    # @todo don't allow other domains, or rather check them
-    # but don't parse them for other links
-
     urls.append({
         "url": url,
         "parsed": False,
         "code": None,
+        "error": None,
     })
 
 
 def next_url():
-    return next(url for url in urls if url["parsed"] == False)
+    try:
+        return next(url for url in urls if url["parsed"] == False)
+    except StopIteration:
+        return None
 
 
-def parse_url(url):
-    r = requests.get(url)
-    parser.feed(r.text)
+def handle_url(url):
+    print("Checking", url["url"], "...", end="")
+
+    url["parsed"] = True
+
+    # Get content
+    try:
+        r = requests.get(url["url"])
+    except requests.exceptions.InvalidURL:
+        url["error"] = "Invalid URL"
+        print("")
+        return
+    except requests.exceptions.ConnectionError:
+        url["error"] = "Connection error"
+        print("")
+        return
+    except requests.exceptions.MissingSchema:
+        url["error"] = "Missing schema"
+        print("")
+        return
+
+    url["code"] = r.status_code
+    print(r.status_code)
+
+    # If non-200, log error and bail
+    if r.status_code != 200:
+        return
+
+    url_parts = urlparse(url["url"])
+
+    # Parse page for content
+    if url_parts.netloc == start_url_parts.netloc:
+        parser.feed(r.text)
 
 
 def run():
     url = next_url()
 
     while url:
-        print("Checking", url)
+        handle_url(url)
         url = next_url()
 
+    print("")
+    print("All done!")
+    print(urls)
 
-add_url(args.url)
+
+start_url = args.url
+start_url_parts = urlparse(start_url)
+base_url = start_url_parts.scheme + "://" + start_url_parts.netloc
+
+add_url(start_url)
 run()
